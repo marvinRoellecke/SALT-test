a1c56be0d55776efd3205378929d1d8853abb976
Marvin Roellecke <marvin@roellecke.com>
2023-01-06 13:04:44 +0100

    create basic form with all requested entries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
 
 function App() {
-  return <h1>Hello World</h1>;
+  function handleSubmit(event) {
+    event.preventDefault();
+    event.target.reset();
+  }
+
+  return (
+    <>
+      <header>
+        <h1 id="formTitle">Dev Track</h1>
+      </header>
+      <main>
+        <form aria-labelledby="formTitle" onSubmit={handleSubmit}>
+          <h2>add new developer</h2>
+          <fieldset>
+            <legend>developer data</legend>
+            <label htmlFor="firstName">First Name:</label>
+            <input type="text" name="firstName" id="firstName" required />
+            <label htmlFor="lastName">Last Name:</label>
+            <input type="text" name="lastName" id="lastName" required />
+            <label htmlFor="bootcamp">Select bootcamp:</label>
+            <select name="bootcamp" id="bootcamp" required>
+              <option value="all">all</option>
+              <option value="jsfs">jsfs</option>
+              <option value="dnfs">dnfs</option>
+              <option value="jfs">jfs</option>
+            </select>
+          </fieldset>
+          <button type="submit">Add Developer</button>
+        </form>
+        <section>gallery</section>
+      </main>
+      <footer>(c) 2023 </footer>
+    </>
+  );
 }
 
 export default App;
